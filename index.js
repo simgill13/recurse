6e48517ab435d279dddfea6811355bad83760b82
simgill13 <simgill89@gmail.com>
2020-05-05 18:03:31 -0700

    1 file modified, 1 deleted

--- index.js
+++ index.js
@@ -177,23 +177,35 @@ function capitalizeFirst(array) {
     empty.push(arry.join(""));
   }
 
-  console.log(empty)
-  return empty
-  
+  console.log(empty);
+  return empty;
 }
 // capitalizeFirst(["car", "taco", "banana"]);
 
 // capitalizeFirst(['car','taco','banana']); // ['Car','Taco','Banana']
 
-
-
-
-
-
-function nestedEvenSum () {
-  // add whatever parameters you deem necessary - good luck!
+function nestedEvenSum(obj) {
+  let sum = 0;
+  let recurse = o => {
+    if (Object.keys(o).length === 0) return;
+    for (key in o) {
+      let val = o[key];
+      if (typeof o[key] == "object") {
+        recurse(val);
+      } else {
+        if (typeof val === "number") {
+          let odd = isOdd(val);
+          if (!odd) {
+            sum += val;
+          }
+        }
+      }
+    }
+  };
+  recurse(obj);
+  console.log(sum);
+  return sum;
 }
-
 
 var obj1 = {
   outer: 2,
@@ -205,15 +217,15 @@ var obj1 = {
       alsoNotANumber: "yup"
     }
   }
-}
+};
 
 var obj2 = {
   a: 2,
-  b: {b: 2, bb: {b: 3, bb: {b: 2}}},
-  c: {c: {c: 2}, cc: 'ball', ccc: 5},
+  b: { b: 2, bb: { b: 3, bb: { b: 2 } } },
+  c: { c: { c: 2 }, cc: "ball", ccc: 5 },
   d: 1,
-  e: {e: {e: 2}, ee: 'car'}
+  e: { e: { e: 2 }, ee: "car" }
 };
 
-nestedEvenSum(obj1); // 6
-nestedEvenSum(obj2); // 10
+// nestedEvenSum(obj1); // 6
+// nestedEvenSum(obj2); // 10
